0b51747c26d25e6f5670bc8ef873d3b0c45c71eb
chakornpat <chakornpat0206@outlook.com>
2025-04-08 17:32:08 +0700

    Implement initial Axum HTTP server with health check and not found routes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::sync::Arc;
 use rust_quest_tracker::config::{config_loader, config_models::DotEnvyConfig};
 use rust_quest_tracker::infrastructure::postgres::{postgres_connector};
+use rust_quest_tracker::infrastructure::axum_http::{http_serve};
 use tracing::{error, info};
 
 #[tokio::main]
@@ -27,4 +29,10 @@ async fn main() {
     };
 
     info!("Postgres connection pool has been established");
+
+    http_serve::start(Arc::new(dotenvy_env), Arc::new(postgres_pool))
+        .await
+        .expect("Failed to start the server");
+    
+
 }
