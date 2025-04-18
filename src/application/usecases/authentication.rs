8af741f5106b37ff7dfed47d80750c4565686e7a
chakornpat <chakornpat0206@outlook.com>
2025-04-18 20:09:28 +0700

    Implement JWT authentication models and update authentication use cases

--- src/application/usecases/authentication.rs
+++ src/application/usecases/authentication.rs
@@ -1,18 +1,25 @@
+use crate::{
+    domain::repositories::{
+        adventurers::AdventurersRepository, guild_commanders::GuildCommandersRepository,
+    },
+    infrastructure::jwt_authentication::{authentication_model::LoginModel, jwt_model::Passport},
+};
+use anyhow::Result;
 use std::sync::Arc;
 
-use crate::domain::repositories::{adventurers::AdventurersRepository, guild_commanders::GuildCommandersRepository};
-
-pub struct AuthenticationUsecase<T1,T2> where
+pub struct AuthenticationUsecase<T1, T2>
+where
     T1: AdventurersRepository + Send + Sync,
-    T2:GuildCommandersRepository + Send + Sync,
+    T2: GuildCommandersRepository + Send + Sync,
 {
     adventurers_repository: Arc<T1>,
     crew_switchboard_repository: Arc<T2>,
 }
 
-impl<T1,T2> AuthenticationUsecase<T1,T2> where
+impl<T1, T2> AuthenticationUsecase<T1, T2>
+where
     T1: AdventurersRepository + Send + Sync,
-    T2:GuildCommandersRepository + Send + Sync,
+    T2: GuildCommandersRepository + Send + Sync,
 {
     pub fn new(adventurers_repository: Arc<T1>, crew_switchboard_repository: Arc<T2>) -> Self {
         AuthenticationUsecase {
@@ -21,20 +28,19 @@ impl<T1,T2> AuthenticationUsecase<T1,T2> where
         }
     }
 
-    pub async fn adventurer_login(&self) {
+    pub async fn adventurer_login(&self, loginModel: LoginModel) -> Result<Passport> {
         unimplemented!()
     }
 
-    pub async fn adventurer_refresh_token(&self) {
+    pub async fn adventurer_refresh_token(&self, refresh_token: String) -> Result<Passport> {
         unimplemented!()
     }
 
-    pub async fn guild_commander_login(&self) {
+    pub async fn guild_commander_login(&self, loginModel: LoginModel) -> Result<Passport> {
         unimplemented!()
     }
 
-    pub async fn guild_commander_refresh_token(&self) {
+    pub async fn guild_commander_refresh_token(&self, refresh_token: String) -> Result<Passport> {
         unimplemented!()
     }
-
 }
--- src/infrastructure/axum_http/routers/authentication.rs
+++ src/infrastructure/axum_http/routers/authentication.rs
@@ -1,16 +1,20 @@
 use std::sync::Arc;
 
-use axum::{extract::State, response::IntoResponse, routing::post, Router};
+use axum::{extract::State, response::IntoResponse, routing::post, Json, Router};
+use axum_extra::extract::CookieJar;
 
 use crate::{
     application::usecases::authentication::{self, AuthenticationUsecase},
     domain::repositories::{
         adventurers::AdventurersRepository, guild_commanders::GuildCommandersRepository,
     },
-    infrastructure::postgres::{
-        postgres_connector::PgPoolSquad,
-        repositories::{
-            adventurers::AdventurerPostgres, guild_commanders::GuildCommandersPostgres,
+    infrastructure::{
+        jwt_authentication::authentication_model::LoginModel,
+        postgres::{
+            postgres_connector::PgPoolSquad,
+            repositories::{
+                adventurers::AdventurerPostgres, guild_commanders::GuildCommandersPostgres,
+            },
         },
     },
 };
@@ -23,14 +27,21 @@ pub fn routes(db_pool: Arc<PgPoolSquad>) -> Router {
 
     Router::new()
         .route("/adventurers/login", post(adventurers_login))
-        .route("/adventurers/refresh-token", post(adventurers_refresh_token))
+        .route(
+            "/adventurers/refresh-token",
+            post(adventurers_refresh_token),
+        )
         .route("/guild_commanders/login", post(guild_commanders_login))
-        .route("/guild_commanders/refresh-token", post(guild_commanders_refresh_token))
+        .route(
+            "/guild_commanders/refresh-token",
+            post(guild_commanders_refresh_token),
+        )
         .with_state(Arc::new(authentication_use_case))
 }
 
 pub async fn adventurers_login<T1, T2>(
     State(authentication_use_case): State<Arc<AuthenticationUsecase<T1, T2>>>,
+    Json(login_model): Json<LoginModel>,
 ) -> impl IntoResponse
 where
     T1: AdventurersRepository + Send + Sync,
@@ -39,10 +50,11 @@ where
     unimplemented!()
 }
 
-pub async fn adventurers_refresh_token<T1,T2>(
+pub async fn adventurers_refresh_token<T1, T2>(
     State(authentication_use_case): State<Arc<AuthenticationUsecase<T1, T2>>>,
+    jar: CookieJar,
 ) -> impl IntoResponse
-where 
+where
     T1: AdventurersRepository + Send + Sync,
     T2: GuildCommandersRepository + Send + Sync,
 {
@@ -50,7 +62,8 @@ where
 }
 
 pub async fn guild_commanders_login<T1, T2>(
-    State(authentication_use_case): State<Arc<AuthenticationUsecase<T1, T2>>>
+    State(authentication_use_case): State<Arc<AuthenticationUsecase<T1, T2>>>,
+    Json(loginModel): Json<LoginModel>,
 ) -> impl IntoResponse
 where
     T1: AdventurersRepository + Send + Sync,
@@ -60,7 +73,8 @@ where
 }
 
 pub async fn guild_commanders_refresh_token<T1, T2>(
-    State(authentication_use_case): State<Arc<AuthenticationUsecase<T1, T2>>>
+    State(authentication_use_case): State<Arc<AuthenticationUsecase<T1, T2>>>,
+    jar: CookieJar,
 ) -> impl IntoResponse
 where
     T1: AdventurersRepository + Send + Sync,
